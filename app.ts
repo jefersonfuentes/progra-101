efb1636975eedec4d20331a89941225e6b5ec3d2
Jeferson <jeferson.fg710@gmail.com>
2023-03-30 17:26:38 -0600

    sections folder added

--- app.ts
+++ app.ts
@@ -1,18 +1,2 @@
-let numberOfLines = 1;
 
-//  Línea #1
-console.log('Línea #', numberOfLines);
-
-// Incremento Línea #2
-numberOfLines++;
-
-console.log('Línea #', numberOfLines);
-// Incremento Línea #3
-numberOfLines++;
-
-console.log('Línea #', numberOfLines);
-// Incremento Línea #4
-numberOfLines++;
-
-console.log('Línea #', numberOfLines);
-
+console.log('Hola Mundo')
---
+++ sections/section-2.ts
@@ -0,0 +1,13 @@
+let numberOfLines = 1;
+
+export function printLineNumber() {
+    // numberOfLines = numberOfLines + 3;
+    console.log('Línea #', numberOfLines);
+    numberOfLines ++;
+}
+
+printLineNumber()
+printLineNumber()
+printLineNumber()
+printLineNumber()
+
