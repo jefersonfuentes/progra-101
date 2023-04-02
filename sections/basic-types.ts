5edb1fbb62ec250945ec4632d3fd9259cd2b2e28
Jeferson <jeferson.fg710@gmail.com>
2023-04-01 22:00:08 -0600

    data types

---
+++ sections/basic-types.ts
@@ -0,0 +1,16 @@
+// Strings
+
+export let firstName: string = 'Andrew';
+export let lastName: string = 'Fuentes'
+
+// console.log( firstName, lastName);
+let fullName: string = firstName + ' ' + lastName;
+
+export let number1: number = 10.5;
+export let number2: number = 20.3;
+
+console.log( number1 + number2);
+
+export let isActive: boolean = true;
+export let isRunning: boolean = false;
+export let hasKids: boolean = true;
